6c084057f2b5ca45d23c6054cca05d834e7e0903
Jwiroo <qorudgus77@gmail.com>
2026-02-07 17:26:56 +0900

    feat: add cors

--- core/config.py
+++ core/config.py
@@ -3,5 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# DB 인증
 supabase_url = os.getenv("SUPABASE_URL")
 supabase_key = os.getenv("SUPABASE_KEY")
+
+# CORS URL
+cors_origins = os.getenv("CORS_ORIGINS").split(",")
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from routers import recommends
+from core.config import cors_origins
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=cors_origins,
+  allow_methods=["*"],
+  allow_headers=["*"]
+)
 
 # routers 연결
 app.include_router(recommends.router)
